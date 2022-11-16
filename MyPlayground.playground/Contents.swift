ccc7d8f867effb362ab571f15348b42974c80cdd
yanyihu <yanyihu@tencent.com>
2022-11-16 09:47:29 +0800

    Added A Better Solution For Count Nodes

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,7 +5,7 @@
 class TwoSum {
     
     
-    // My answer, DFS + recursion
+    // My answer, hash table + two iterations
     // T=O(n), S=O(n)
     func solution1(_ nums: [Int], _ target: Int) -> [Int] {
         var dict = Dictionary<Int, Int>()
@@ -58,7 +58,8 @@ class CountNodes {
         }
     }
     
-    // My answer, T=O(N), S=O(N)
+    // My answer, DFS + recursion
+    // T=O(N), S=O(logN)
     func solution1(_ root: TreeNode?) -> Int {
         var sum = 0
         if root == nil {
@@ -76,10 +77,78 @@ class CountNodes {
         
         return sum
     }
+    
+    // solution 1 but cleaner
+    // https://leetcode.com/problems/count-complete-tree-nodes/solutions/265456/swift-two-line-solution/
+    func solution1_2(_ root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        return 1 + solution1_2(root.left) + solution1_2(root.right)
+    }
+    
+    // find the height of the tree, T=O(log(N)), S=O(log(N))
+    func treeHeight(_ root: TreeNode?) -> Int {
+        // base case
+        guard let root = root else {
+            return 0
+        }
+        
+        // because complete tree, left node must exist before right node does
+        if root.right != nil {
+            return treeHeight(root.right) + 1
+        } else {
+            return treeHeight(root.left) + 1
+        }
+    }
+    
+    // my answer using dc, but it doesn't work
+    func solution2(_ root: TreeNode?) -> Int {
+        // base case
+        guard let root = root else {
+//            print("root nil, so 0")
+            return 0
+        }
+        
+        // because complete tree, left node must exist before right node does
+        if let right = root.right {
+            // right exist, so left must be as deep as right
+            return ((1 << treeHeight(right)) - 1) + solution2(right) + 1
+        } else {
+            // right does not exist, so the count is left + 1
+            return solution2(root.left) + 1
+        }
+    }
+    
+    // the correct answer, T=O(logN*logN), S=O(logN)
+    // https://leetcode.com/problems/count-complete-tree-nodes/solutions/2815541/c-dfs-optimized-faster-easy-to-understand/
+    func solution2_1(_ root: TreeNode?) -> Int {
+        // base case
+        guard let root = root else {
+            return 0
+        }
+        
+        var l = 1, r = 1
+        var left = root.left, right = root.right
+        while left != nil {
+            left = left!.left
+            l += 1
+        }
+        while right != nil {
+            right = right!.right
+            r += 1
+        }
+        
+        if l == r {
+            return (1 << l) - 1
+        }
+        
+        return solution2_1(root.left) + solution2(root.right) + 1
+    }
 }
 
 func countNodesTest() {
-    print("twoSum test:");
+    print("countNodes test:");
     
     do {
         let n6 = CountNodes.TreeNode(6, nil, nil)
@@ -89,17 +158,43 @@ func countNodesTest() {
         let n2 = CountNodes.TreeNode(2, n4, n5)
         let n1 = CountNodes.TreeNode(1, n2, n3)
         
-        print("case 1: ", CountNodes().solution1(n1)) // 6
+        print("case 1: ", CountNodes().solution2_1(n1)) // 6
     }
     
     do {
-        print("case 2: ", CountNodes().solution1(nil)) // 0
+        print("case 2: ", CountNodes().solution2_1(nil)) // 0
     }
     
     do {
         let n1 = CountNodes.TreeNode(1, nil, nil)
         
-        print("case 3: ", CountNodes().solution1(n1)) // 1
+        print("case 3: ", CountNodes().solution2_1(n1)) // 1
+    }
+    
+    do {
+        let n2 = CountNodes.TreeNode(2, nil, nil)
+        let n1 = CountNodes.TreeNode(1, n2, nil)
+        
+        print("case 4: ", CountNodes().solution2_1(n1)) // 2
+    }
+    
+    do {
+        let n4 = CountNodes.TreeNode(4, nil, nil)
+        let n3 = CountNodes.TreeNode(3, nil, nil)
+        let n2 = CountNodes.TreeNode(2, n4, nil)
+        let n1 = CountNodes.TreeNode(1, n2, n3)
+        
+        print("case 5: ", CountNodes().solution2_1(n1)) // 4
+    }
+    
+    do {
+        let n5 = CountNodes.TreeNode(5, nil, nil)
+        let n4 = CountNodes.TreeNode(4, nil, nil)
+        let n3 = CountNodes.TreeNode(3, nil, nil)
+        let n2 = CountNodes.TreeNode(2, n4, n5)
+        let n1 = CountNodes.TreeNode(1, n2, n3)
+        
+        print("case 6: ", CountNodes().solution2_1(n1)) // 5
     }
 }
 
